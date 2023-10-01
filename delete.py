ec621323c030b833d24dfc18d9e64e57b36f056d
2naive <to.naive@gmail.com>
2023-10-01 20:57:27 +0300

    +delete messages

--- delete.py
+++ delete.py
@@ -2,17 +2,17 @@ import urllib.request, json, os, sys, time, re
 from datetime import datetime
 
 V = "5.131"
+TYPES = ['video', 'photos', 'wall', 'messages', 'friends']
 DELAY = 0.25 # https://dev.vk.com/ru/api/api-requests#%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%B5%D0%BD%D0%B8%D1%8F
 OFFSET_MAX = 20
 
-if len( sys.argv ) < 3:
-    print("python3 delete.py [video|photos|wall]")
+if len( sys.argv ) < 2:
+    print(f"python3 delete.py [{'|'.join(TYPES)}]")
     print("# Help: https://github.com/2naive/vk-download/tree/master#readme")
     # https://oauth.vk.com/authorize?client_id=51756670&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,video&response_type=token&v=5.131&state=123456
     sys.exit()
 
 TOKEN = sys.argv[1]
-CMD = sys.argv[2]
 
 with urllib.request.urlopen(f"https://api.vk.com/method/users.get?access_token={TOKEN}&v={V}") as url:
     data = json.load(url)
@@ -34,6 +34,13 @@ def delete_photos(token):
 
 def delete_wall(token):
     delete_items(token, 'wall.get', 'wall.delete', 'post')
+
+def delete_friends(token):
+    delete_items(token, 'friends.get', 'friends.delete', 'user')
+    delete_items(token, 'friends.getRequests', 'friends.delete', 'user')
+
+def delete_messages(token):
+    delete_items(token, 'messages.get', 'messages.delete', 'message')
 
 def delete_items(token, method_list, method_delete, item_name, offset=OFFSET_MAX):
     time.sleep(DELAY)
@@ -59,5 +66,9 @@ def delete_items(token, method_list, method_delete, item_name, offset=OFFSET_MAX
                 print(f"[{i}] {item['id']}")
         if offset < count:
             delete_items(token, method_list, method_delete, item_name, offset)
-
-globals()[f"delete_{CMD}"](TOKEN)
+if len( sys.argv ) < 3:
+    for item in TYPES:
+        globals()[f"delete_{item}"](TOKEN)
+else:
+    CMD = sys.argv[2]
+    globals()[f"delete_{CMD}"](TOKEN)
--- friends.py
+++ friends.py
@@ -1,17 +1,19 @@
 import urllib.request, json, os, sys, time, re
 
 V = "5.131"
-DELAY = 1.01
+DELAY = 1.01 # https://dev.vk.com/ru/api/api-requests#%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%B5%D0%BD%D0%B8%D1%8F
 
 if len( sys.argv ) < 3:
     print("python3 friends.py TOKEN UID [DEPTH=1]")
     print("# TOKEN howto: https://github.com/2naive/vk-download/tree/master#readme")
-    # https://oauth.vk.com/authorize?client_id=51756670&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,video&response_type=token&v=5.131&state=123456
+    # https://oauth.vk.com/authorize?client_id=51756670&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends,photos,video,messages,wall&response_type=token&v=5.131&state=123457
     sys.exit()
 TOKEN = sys.argv[1]
 UID = sys.argv[2]
 LIMIT = 1 if len( sys.argv ) < 4 else int(sys.argv[3])
-print(f"Started: TOKEN={TOKEN} UID={UID} DEPTH={LIMIT}")
+
+TOKEN_MASKED = "{}...{}".format(TOKEN[0:12], TOKEN[-8:])
+print(f"Started: TOKEN={TOKEN_MASKED} UID={UID}")
 
 def dump_user(uid, path="."):
     level = path.count('/') + 1
@@ -29,12 +31,12 @@ def dump_user(uid, path="."):
         if not lock(path, uid):
             return
 
-    time.sleep(DELAY) # https://dev.vk.com/ru/api/api-requests#%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%B5%D0%BD%D0%B8%D1%8F
+    time.sleep(DELAY)
     with urllib.request.urlopen(f"https://api.vk.com/method/users.get?access_token={TOKEN}&user_id={uid}&v={V}&order=name&fields=activities,about,blacklisted,blacklisted_by_me,books,bdate,can_be_invited_group,can_post,can_see_all_posts,can_see_audio,can_send_friend_request,can_write_private_message,career,common_count,connections,contacts,city,country,crop_photo,domain,education,exports,followers_count,friend_status,has_photo,has_mobile,home_town,photo_100,photo_200,photo_200_orig,photo_400_orig,photo_50,sex,site,schools,screen_name,status,verified,games,interests,is_favorite,is_friend,is_hidden_from_feed,last_seen,maiden_name,military,movies,music,nickname,occupation,online,personal,photo_id,photo_max,photo_max_orig,quotes,relation,relatives,timezone,tv,universities&count=") as url:
         data = json.load(url)
         if "error" in data:
             print(f"— Error @users.get {data['error']['error_code']}: {data['error']['error_msg']}")
-            if data['error']['error_code'] == 29:
+            if data['error']['error_code'] in [29,5]:
                 sys.exit()
             unlock(path)
             return
@@ -44,17 +46,17 @@ def dump_user(uid, path="."):
         with open(f"{path}/{fname}.profile.json", 'w', encoding='utf-8') as f:
             json.dump(profile, f, ensure_ascii=False, indent=2)
         try:
-            time.sleep(DELAY) # https://dev.vk.com/ru/api/api-requests#%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%B5%D0%BD%D0%B8%D1%8F
+            time.sleep(DELAY)
             urllib.request.urlretrieve(profile['photo_max_orig'], f"{path}/{fname}.jpg")
         except:
             print('[x] No photo')
 
-    time.sleep(DELAY) # https://dev.vk.com/ru/api/api-requests#%D0%A7%D0%B0%D1%81%D1%82%D0%BE%D1%82%D0%BD%D1%8B%D0%B5%20%D0%BE%D0%B3%D1%80%D0%B0%D0%BD%D0%B8%D1%87%D0%B5%D0%BD%D0%B8%D1%8F
+    time.sleep(DELAY)
     with urllib.request.urlopen(f"https://api.vk.com/method/friends.get?access_token={TOKEN}&user_id={uid}&v={V}&order=name&fields=bdate,can_post,can_see_all_posts,can_write_private_message,city,contacts,country,domain,education,has_mobile,timezone,last_seen,nickname,online,photo_100,photo_200_orig,photo_50,relation,sex,status,universities&count=") as url:
         data = json.load(url)
         if "error" in data:
             print(f"— Error @friends.get {data['error']['error_code']}: {data['error']['error_msg']}")
-            if data['error']['error_code'] == 29:
+            if data['error']['error_code'] in [29,5]:
                 sys.exit()
             unlock(path)
             return
